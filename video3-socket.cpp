270db27fc1275bc4c63bd8fd5e36bd3bee0bc1d6
fchis <felicia_chis_08@yahoo.com>
2016-12-22 18:39:27 +0300

    Create video3-socket.cpp

---
+++ video3-socket.cpp
@@ -0,0 +1,405 @@
+#include <sstream>
+#include <string>
+#include <iostream>
+//#include <opencv2\highgui.h>
+#include "opencv2/highgui/highgui.hpp"
+//#include <opencv2\cv.h>
+#include "opencv2/opencv.hpp"
+
+//socket 1/4
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
+using namespace std;
+using namespace cv;
+//initial min and max HSV filter values.
+//these will be changed using trackbars
+int H_MIN = 0;
+int H_MAX = 256;
+int S_MIN = 0;
+int S_MAX = 256;
+int V_MIN = 0;
+int V_MAX = 256;
+
+int H_MIN_B = 41;
+int H_MAX_B = 256;
+int S_MIN_B = 117;
+int S_MAX_B = 256;
+int V_MIN_B = 0;
+int V_MAX_B = 256;
+
+int H_MIN_R = 0;
+int H_MAX_R = 41;
+int S_MIN_R = 128;
+int S_MAX_R = 245;
+int V_MIN_R = 66;
+int V_MAX_R = 256;
+
+//default capture width and height
+const int FRAME_WIDTH = 640;
+const int FRAME_HEIGHT = 480;
+//max number of objects to be detected in frame
+const int MAX_NUM_OBJECTS = 50;
+//minimum and maximum object area
+const int MIN_OBJECT_AREA = 20 * 20;
+const int MAX_OBJECT_AREA = FRAME_HEIGHT*FRAME_WIDTH / 1.5;
+//names that will appear at the top of each window
+const std::string windowName = "Original Image";
+const std::string windowName1 = "HSV Image";
+const std::string windowName2 = "Thresholded Image";
+const std::string windowName3 = "After Morphological Operations";
+const std::string trackbarWindowName = "Trackbars";
+
+
+void on_mouse(int e, int x, int y, int d, void *ptr)
+{
+	if (e == EVENT_LBUTTONDOWN)
+	{
+		cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+	}
+}
+
+void on_trackbar(int, void*)
+{//This function gets called whenever a
+ // trackbar position is changed
+}
+
+string intToString(int number) {
+
+
+	std::stringstream ss;
+	ss << number;
+	return ss.str();
+}
+
+void createTrackbars() {
+	//create window for trackbars
+
+
+	namedWindow(trackbarWindowName, 0);
+	//create memory to store trackbar name on window
+	char TrackbarName[50];
+ /*
+	sprintf(TrackbarName, "H_MIN", H_MIN);
+	sprintf(TrackbarName, "H_MAX", H_MAX);
+	sprintf(TrackbarName, "S_MIN", S_MIN);
+	sprintf(TrackbarName, "S_MAX", S_MAX);
+	sprintf(TrackbarName, "V_MIN", V_MIN);
+	sprintf(TrackbarName, "V_MAX", V_MAX);*/
+	//create trackbars and insert them into window
+	//3 parameters are: the address of the variable that is changing when the trackbar is moved(eg.H_LOW),
+	//the max value the trackbar can move (eg. H_HIGH),
+	//and the function that is called whenever the trackbar is moved(eg. on_trackbar)
+	//                                  ---->    ---->     ---->
+	createTrackbar("H_MIN", trackbarWindowName, &H_MIN, H_MAX, on_trackbar);
+	createTrackbar("H_MAX", trackbarWindowName, &H_MAX, H_MAX, on_trackbar);
+	createTrackbar("S_MIN", trackbarWindowName, &S_MIN, S_MAX, on_trackbar);
+	createTrackbar("S_MAX", trackbarWindowName, &S_MAX, S_MAX, on_trackbar);
+	createTrackbar("V_MIN", trackbarWindowName, &V_MIN, V_MAX, on_trackbar);
+	createTrackbar("V_MAX", trackbarWindowName, &V_MAX, V_MAX, on_trackbar);
+
+
+}
+void drawObject(int x, int y, Mat &frame) {
+
+	//use some of the openCV drawing functions to draw crosshairs
+	//on your tracked image!
+
+	//UPDATE:JUNE 18TH, 2013
+	//added 'if' and 'else' statements to prevent
+	//memory errors from writing off the screen (ie. (-25,-25) is not within the window!)
+
+	circle(frame, Point(x, y), 20, Scalar(0, 255, 0), 2);
+	if (y - 25 > 0)
+		line(frame, Point(x, y), Point(x, y - 25), Scalar(0, 255, 0), 2);
+	else line(frame, Point(x, y), Point(x, 0), Scalar(0, 255, 0), 2);
+	if (y + 25 < FRAME_HEIGHT)
+		line(frame, Point(x, y), Point(x, y + 25), Scalar(0, 255, 0), 2);
+	else line(frame, Point(x, y), Point(x, FRAME_HEIGHT), Scalar(0, 255, 0), 2);
+	if (x - 25 > 0)
+		line(frame, Point(x, y), Point(x - 25, y), Scalar(0, 255, 0), 2);
+	else line(frame, Point(x, y), Point(0, y), Scalar(0, 255, 0), 2);
+	if (x + 25 < FRAME_WIDTH)
+		line(frame, Point(x, y), Point(x + 25, y), Scalar(0, 255, 0), 2);
+	else line(frame, Point(x, y), Point(FRAME_WIDTH, y), Scalar(0, 255, 0), 2);
+
+	putText(frame, intToString(x) + "," + intToString(y), Point(x, y + 30), 1, 1, Scalar(0, 255, 0), 2);
+	//cout << "x,y: " << x << ", " << y;
+
+}
+void morphOps(Mat &thresh) {
+
+	//create structuring element that will be used to "dilate" and "erode" image.
+	//the element chosen here is a 3px by 3px rectangle
+
+	Mat erodeElement = getStructuringElement(MORPH_RECT, Size(3, 3));
+	//dilate with larger element so make sure object is nicely visible
+	Mat dilateElement = getStructuringElement(MORPH_RECT, Size(8, 8));
+
+	erode(thresh, thresh, erodeElement);
+	erode(thresh, thresh, erodeElement);
+
+
+	dilate(thresh, thresh, dilateElement);
+	dilate(thresh, thresh, dilateElement);
+
+
+
+}
+void trackFilteredObject(int &x, int &y, Mat threshold, Mat &cameraFeed) {
+
+	Mat temp;
+	threshold.copyTo(temp);
+	//these two vectors needed for output of findContours
+	vector< vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+	//find contours of filtered image using openCV findContours function
+	findContours(temp, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
+	//use moments method to find our filtered object
+	double refArea = 0;
+	bool objectFound = false;
+	if (hierarchy.size() > 0) {
+		int numObjects = hierarchy.size();
+		//if number of objects greater than MAX_NUM_OBJECTS we have a noisy filter
+		if (numObjects < MAX_NUM_OBJECTS) {
+			for (int index = 0; index >= 0; index = hierarchy[index][0]) {
+
+				Moments moment = moments((cv::Mat)contours[index]);
+				double area = moment.m00;
+
+				//if the area is less than 20 px by 20px then it is probably just noise
+				//if the area is the same as the 3/2 of the image size, probably just a bad filter
+				//we only want the object with the largest area so we safe a reference area each
+				//iteration and compare it to the area in the next iteration.
+				if (area > MIN_OBJECT_AREA && area<MAX_OBJECT_AREA && area>refArea) {
+					x = moment.m10 / area;
+					y = moment.m01 / area;
+					objectFound = true;
+					refArea = area;
+				}
+				else objectFound = false;
+
+
+			}
+			//let user know you found an object
+			if (objectFound == true) {
+				putText(cameraFeed, "Tracking Object", Point(0, 50), 2, 1, Scalar(0, 255, 0), 2);
+				//draw object location on screen
+				//cout << x << "," << y;
+				drawObject(x, y, cameraFeed);
+
+			}
+
+
+		}
+		else putText(cameraFeed, "TOO MUCH NOISE! ADJUST FILTER", Point(0, 50), 1, 2, Scalar(0, 0, 255), 2);
+	}
+}
+
+
+// socket 2/4
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+// /socket
+
+
+int main(int argc, char* argv[])
+{
+
+	//some boolean variables for different functionality within this
+	//program
+	bool trackObjects = true;
+	bool useMorphOps = true;
+
+	Point p;
+	//Matrix to store each frame of the webcam feed
+	Mat cameraFeed;
+	//matrix storage for HSV image
+	Mat HSV;
+	//matrix storage for binary threshold image
+	Mat threshold;
+	//x and y values for the location of the object
+	int x = 0, y = 0;
+	//create slider bars for HSV filtering
+	createTrackbars();
+	//video capture object to acquire webcam feed
+	VideoCapture capture;
+	//open capture object at location zero (default location for webcam)
+	capture.open("rtmp://172.16.254.63/live/live");
+	//set height and width of capture frame
+	capture.set(CV_CAP_PROP_FRAME_WIDTH, FRAME_WIDTH);
+	capture.set(CV_CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT);
+	//start an infinite loop where webcam feed is copied to cameraFeed matrix
+	//all of our operations will be performed within this loop
+
+
+
+
+
+
+
+
+
+// begin socket 3/4
+    int sockfd, newsockfd, portno;
+     socklen_t clilen;
+     char buffer[256];
+     struct sockaddr_in serv_addr, cli_addr;
+     int n;
+     
+  // create a socket
+     // socket(int domain, int type, int protocol)
+     sockfd =  socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) 
+        error("ERROR opening socket");
+        
+     // clear address structure
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+
+     portno = 20231;
+
+     /* setup the host_addr structure for use in bind call */
+     // server byte order
+     serv_addr.sin_family = AF_INET;  
+
+     // automatically be filled with current host's IP address
+     serv_addr.sin_addr.s_addr = INADDR_ANY;  
+
+     // convert short integer value for port must be converted into network byte order
+     serv_addr.sin_port = htons(portno);
+
+     // bind(int fd, struct sockaddr *local_addr, socklen_t addr_length)
+     // bind() passes file descriptor, the address structure, 
+     // and the length of the address structure
+     // This bind() call will bind  the socket to the current IP address on port, portno
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0) 
+              error("ERROR on binding");
+
+     // This listen() call tells the socket to listen to the incoming connections.
+     // The listen() function places all incoming connection into a backlog queue
+     // until accept() call accepts the connection.
+     // Here, we set the maximum size for the backlog queue to 5.
+     listen(sockfd,5);
+
+     // The accept() call actually accepts an incoming connection
+     clilen = sizeof(cli_addr);
+
+     // This accept() function will write the connecting client's address info 
+     // into the the address structure and the size of that structure is clilen.
+     // The accept() returns a new socket file descriptor for the accepted connection.
+     // So, the original socket file descriptor can continue to be used 
+     // for accepting new connections while the new socker file descriptor is used for
+     // communicating with the connected client.
+     newsockfd = accept(sockfd, 
+                 (struct sockaddr *) &cli_addr, &clilen);
+     if (newsockfd < 0) 
+          error("ERROR on accept");
+
+     printf("server: got connection from %s port %d\n",
+            inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
+
+
+     // This send() function sends the 13 bytes of the string to the new socket
+     send(newsockfd, "Hello, world!\n", 13, 0);
+
+     bzero(buffer,256);
+
+     n = read(newsockfd,buffer,255);
+     if (n < 0) error("ERROR reading from socket");
+     printf("Here is the message: %s\n",buffer);
+
+// end socket
+
+
+
+
+
+
+
+	
+	while (1) {
+
+
+		//store image to matrix
+		capture.read(cameraFeed);
+		//convert frame from BGR to HSV colorspace
+   if (cameraFeed.empty())
+   {
+     return 1;
+   }
+   else
+   {
+   
+		cvtColor(cameraFeed, HSV, COLOR_BGR2HSV);
+		//filter HSV image between values and store filtered image to
+		//threshold matrix
+   
+   inRange(HSV, Scalar(H_MIN, S_MIN, V_MIN), Scalar(H_MAX, S_MAX, V_MAX), threshold);
+		//perform morphological operations on thresholded image to eliminate noise
+		//and emphasize the filtered object(s)
+   
+		if (useMorphOps)
+			morphOps(threshold);
+		//pass in thresholded frame to our object tracking function
+		//this function will return the x and y coordinates of the
+		//filtered object
+		if (trackObjects)
+			trackFilteredObject(x, y, threshold, cameraFeed);
+   
+	/*	
+    inRange(HSV, Scalar(H_MIN_B, S_MIN_B, V_MIN_B), Scalar(H_MAX_B, S_MAX_B, V_MAX_B), threshold);
+		//perform morphological operations on thresholded image to eliminate noise
+		//and emphasize the filtered object(s)
+   
+		if (useMorphOps)
+			morphOps(threshold);
+		//pass in thresholded frame to our object tracking function
+		//this function will return the x and y coordinates of the
+		//filtered object
+		if (trackObjects)
+			trackFilteredObject(x, y, threshold, cameraFeed);
+
+    inRange(HSV, Scalar(H_MIN_R, S_MIN_R, V_MIN_R), Scalar(H_MAX_R, S_MAX_R, V_MAX_R), threshold);
+		//perform morphological operations on thresholded image to eliminate noise
+		//and emphasize the filtered object(s)
+   
+		if (useMorphOps)
+			morphOps(threshold);
+		//pass in thresholded frame to our object tracking function
+		//this function will return the x and y coordinates of the
+		//filtered object
+		if (trackObjects)
+			trackFilteredObject(x, y, threshold, cameraFeed);
+   
+   */
+     
+     
+		//show frames
+		imshow(windowName2, threshold);
+		imshow(windowName, cameraFeed);
+		//imshow(windowName1, HSV);
+		setMouseCallback("Original Image", on_mouse, &p);
+		//delay 30ms so that screen can refresh.
+		//image will not appear without this waitKey() command
+		waitKey(30);
+   }
+	}
+
+
+ //soket 4/4
+  close(newsockfd);
+     close(sockfd);
+  
+  
+  
+	return 0;
+}
