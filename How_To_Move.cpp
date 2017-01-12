d7e95c8f5941896eb32f84986cbc2b2a9dc122d2
fchis <felicia_chis_08@yahoo.com>
2017-01-12 15:07:27 +0200

    Create How_To_Move.cpp

---
+++ How_To_Move.cpp
@@ -0,0 +1,43 @@
+int distanta(int x, int y, int x1, int y1)
+{
+	return sqrt((x1-x)*(x1-x)+(y1-y)*(y1-y));
+}
+
+int i=0;
+
+int main()
+{
+
+	//do{---------------------------------------
+	if(i%2==0)
+	{
+		//forward / back
+		buff ="f,s"
+		d=distanta(x,y,x1,y1);
+		//move 
+		if moved
+			new_d=distanta(x,y,x1,y1);
+			
+		if(new_d>d)
+			//buff = "b,s,b,s"
+		else
+			//buff = "f,s"
+	}
+	//	----------------------------------}while( x-x1>10 || x1-x>0 || y-y1>10 || y-y1>10)
+	else{
+		//left / right
+		buff ="l,s"
+		d=distanta(x,y,x1,y1);
+		//move 
+		if moved
+			new_d=distanta(x,y,x1,y1);
+			
+		if(new_d>d)
+			//buff = "r,s,r,s"
+		else
+			//buff = "l,s"
+		
+	}	
+
+
+}
